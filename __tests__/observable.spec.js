2171f5224b3ff5e81fa6584768698d29463b0cfa
ciccibus <domsmasher@live.com>
2018-10-20 11:32:05 +0100

    first test suite

--- __tests__/observable.spec.js
+++ __tests__/observable.spec.js
@@ -5,7 +5,15 @@ import observable from "../src/js/observable";
 describe('Observable', () => {
 	test('Should be an object', () => {
 		const expected = "object";
-		const actual = typeof observable({}, {});
+		const actual = typeof observable({}, []);
   		expect(expected).toEqual(actual);
+	});
+	test('Should call listeners', () => {
+		const myMockListener = jest.fn();
+		const storeObservable = observable({}, [myMockListener]);
+		Reflect.set(storeObservable, "prop", "my prop");
+		expect(myMockListener.mock.calls.length).toBe(1);
+		expect(myMockListener.mock.calls[0][0]).toEqual({});
+		expect(myMockListener.mock.calls[0][1]).toEqual({"prop": "my prop"});
 	});
 });
